d66cbf623f8180725b34affe7955058983804979
hao <1451295544@qq.com>
2021-06-09 16:22:20 +0800

    init

---
+++ app/src/main/java/com/ss/splashutil/MultiDexProUtil.java
@@ -0,0 +1,47 @@
+package com.ss.splashutil;
+
+import android.app.Application;
+import android.widget.TextView;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃  神兽保佑
+ * 　　　　┃　　　┃  代码无bug
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ *
+ * @author hao
+ * @date 2021/6/9
+ * @description
+ */
+
+public class MultiDexProUtil {
+
+   private static Application mApplication;
+
+    public static void init(Application application) {
+        mApplication = application;
+    }
+
+    public static void setTitle() {
+        String x = null;
+        String y = null;
+        String z = x + y;
+        TextView textView = new TextView(mApplication.getApplicationContext());
+        textView.setText(z);
+    }
+}
